0d40159987fb104f954b5ec0d6c8c180dbacbc33
Yleisnero <fischi12325@gmail.com>
2025-07-16 18:08:55 +0200

    Initial commit

---
+++ CityTravelProblem/Program.cs
@@ -0,0 +1,69 @@
+﻿using CityTravelProblem;
+
+var input = "0: 1, 1: 2 3 4, 4: 5";
+
+var graph = """
+                0
+              / |
+             /  1
+             |/ | \
+             2  3  4
+                   |
+                   5
+            """;
+
+Console.WriteLine(input + "\n");
+Console.WriteLine(graph + "\n");
+
+var places = input
+    .Replace(':', ' ')
+    .Replace(',', ' ')
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Distinct()
+    .ToList();
+
+Console.WriteLine("Places: " + string.Join(",", places));
+Console.WriteLine();
+Console.WriteLine("City Matrix:");
+
+var edges = input
+    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+    .Select(part => part.Trim())
+    .Select(part =>
+    {
+        var split = part.Split(':', 2); // split into key : values
+        var key = int.Parse(split[0].Trim());
+        var values = split[1]
+            .Trim()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+        return (key, values);
+    })
+    .ToDictionary(x => x.key, x => x.values);
+
+var city = new CityGraph(places.Count);
+
+foreach (var edge in edges)
+{
+    foreach (var val in edge.Value)
+    {
+        city.AddEdge(edge.Key, val);
+    }
+}
+
+city.DisplayCity();
+
+Console.WriteLine("Shortest Path from 2 to 4: " + string.Join("-", city.ShortestPath(2, 4)));
+Console.WriteLine("Neighbors of 1: " + string.Join(",", city.GetNeighbors(1)));
+Console.WriteLine("Everybody now goes to place 1 \n");
+
+// Everybody goes to place 1
+var target = 1;
+var edgeLoads = city.EdgeLoads(target);
+
+Console.WriteLine($"\nEdge utilisation if everyone goes to {target}:");
+foreach (var kvp in edgeLoads.OrderBy(k => k.Key))
+{
+    Console.WriteLine($"{kvp.Key}: {kvp.Value} people");
+}
